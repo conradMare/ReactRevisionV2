6fd8516b28a4b94d8b8c037c63d26430cfb537aa
conradMare <158014474+conradMare@users.noreply.github.com>
2024-11-09 07:34:59 +0200

    2024/11/09(am)
    Coding Exercise - Building and Using Components - Complete

--- Exercises/building-and-using-a-component/src/App.jsx
+++ Exercises/building-and-using-a-component/src/App.jsx
@@ -4,6 +4,12 @@ import React from 'react';
 // IMPORTANT: Add "export" in front of your component - otherwise the automated tests won't work
 // ...
 
+export function MainGoal() {
+  return (
+    <p>Main goal: Master React</p>
+  )
+}
+
 // DO NOT EDIT THE APP COMPONENT NAME OR CONTENT
 // Except for outputting your custom component
 
@@ -33,6 +39,7 @@ function App() {
       </p>
       {/* DON'T CHANGE THE TEXT / CONTENT ABOVE */}
       {/* OUTPUT YOUR COMPONENT HERE */}
+      <MainGoal />
     </div>
   );
 }
