0d7aef47fa342768760ab0496d2659578faaff2b
conradMare <158014474+conradMare@users.noreply.github.com>
2024-11-09 07:13:47 +0200

    2024/11/09(am)
    Using Vite main.jsx strucure.

--- ReactEssentials-Componenets,JSX,Props,State/react-essentials/src/main.jsx
+++ ReactEssentials-Componenets,JSX,Props,State/react-essentials/src/main.jsx
@@ -1,7 +1,10 @@
-import ReactDOM from "react-dom/client";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-import App from "./App.jsx";
-import "./index.css";
-
-const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
