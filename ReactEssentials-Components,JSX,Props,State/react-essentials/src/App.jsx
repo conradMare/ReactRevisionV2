53166693f21c964f33fb8d77659dd15c18351a90
conradMare <158014474+conradMare@users.noreply.github.com>
2024-11-09 09:30:10 +0200

    2024/11/09(am)
    Alternative Prop Syntaxes - Complete

--- ReactEssentials-Components,JSX,Props,State/react-essentials/src/App.jsx
+++ ReactEssentials-Components,JSX,Props,State/react-essentials/src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
-import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from "./data.js";
 
 function App() {
   return (
@@ -12,16 +12,19 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept
-              title="Components"
-              description="The core UI building block"
-              image={componentsImg}
-              alt="Components image"
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
             />
             <CoreConcept
-              title="Props"
+              {...CORE_CONCEPTS[1]}
             />
-            <CoreConcept />
-            <CoreConcept />
+            <CoreConcept
+              {...CORE_CONCEPTS[2]}
+            />
+            <CoreConcept
+              {...CORE_CONCEPTS[3]}
+            />
           </ul>
         </section>
       </main>
--- ReactEssentials-Components,JSX,Props,State/react-essentials/src/components/CoreConcept.jsx
+++ ReactEssentials-Components,JSX,Props,State/react-essentials/src/components/CoreConcept.jsx
@@ -1,9 +1,9 @@
-export default function CoreConcept(props) {
+export default function CoreConcept({ title, description, image }) {
     return (
         <li>
-            <img src={props.image} alt={props.alt} />
-            <h3>{props.title}</h3>
-            <p>{props.description}</p>
+            <img src={image} />
+            <h3>{title}</h3>
+            <p>{description}</p>
         </li>
     )
 }
