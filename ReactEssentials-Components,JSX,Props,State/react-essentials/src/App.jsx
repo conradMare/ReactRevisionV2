422bad724717769359270ce383db766ef0796c8a
conradMare <158014474+conradMare@users.noreply.github.com>
2024-11-18 09:22:04 +0200

    2024/11/18(am)
    Passing Functions as Values to Props - Complete

--- ReactEssentials-Components,JSX,Props,State/react-essentials/src/App.jsx
+++ ReactEssentials-Components,JSX,Props,State/react-essentials/src/App.jsx
@@ -5,6 +5,10 @@ import { CORE_CONCEPTS } from "./data.js";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  function handleSelect() {
+    console.log('Hello World! - selected');
+  }
+
   return (
     <div>
       <Header />
@@ -31,11 +35,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
           </menu>
+          Dynamic Content
         </section>
       </main>
       <Footer />
