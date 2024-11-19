afcf7817afd23abae1a5d9c683a3538d288aa3f9
conradMare <158014474+conradMare@users.noreply.github.com>
2024-11-19 05:27:24 +0200

    2024/11/19(am)
    Coding Exercise: Component Composition - Updated (Styling)

--- Exercises/component-composition/src/components/Card.jsx
+++ Exercises/component-composition/src/components/Card.jsx
@@ -2,7 +2,7 @@ export default function Card({ name, children }) {
     return (
         <article className="card">
             <h2>{name}</h2>
-            {children}
+            <p><i>{children}</i></p>
         </article>
     );
 }
