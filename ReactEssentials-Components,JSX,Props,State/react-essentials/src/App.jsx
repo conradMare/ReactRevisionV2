64163d5d713de51036db140e45918773249a84f6
conradMare <158014474+conradMare@users.noreply.github.com>
2024-11-25 08:55:45 +0200

    2024/11/25(am)
    Deriving & Outputting Data Based on State - Complete

--- ReactEssentials-Components,JSX,Props,State/react-essentials/src/App.jsx
+++ ReactEssentials-Components,JSX,Props,State/react-essentials/src/App.jsx
@@ -3,10 +3,11 @@ import Footer from "./components/Footer.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import { CORE_CONCEPTS } from "./data.js";
 import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from "./data.js";
 import { useState } from "react";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
@@ -46,7 +47,15 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
       <Footer />
