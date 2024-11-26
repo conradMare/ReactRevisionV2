162fbf268576a6646fb2f0e9d88000827082c582
conradMare <158014474+conradMare@users.noreply.github.com>
2024-11-26 05:45:54 +0200

    2024/11/26(am)
    Rendering Content Conditionally - Complete

--- ReactEssentials-Components,JSX,Props,State/react-essentials/src/components/TabButton.jsx
+++ ReactEssentials-Components,JSX,Props,State/react-essentials/src/components/TabButton.jsx
@@ -1,5 +1,4 @@
 export default function TabButton({ children, onSelect }) {
-    console.log('TAB_BUTTON COMPONENT RENDERING');
     return (
         <li>
             <button onClick={onSelect}>{children}</button>
