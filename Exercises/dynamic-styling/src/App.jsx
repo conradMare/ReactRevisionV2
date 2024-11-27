bb47789221181b444f3fb0c61edf8a9ad643a5c3
conradMare <158014474+conradMare@users.noreply.github.com>
2024-11-27 09:14:55 +0200

    2024/11/27(am)
    Coding Exercise: Dynamic Styling - Format

--- Exercises/dynamic-styling/src/App.jsx
+++ Exercises/dynamic-styling/src/App.jsx
@@ -1,10 +1,10 @@
 // Your task is to dynamically apply a CSS class (active) to the <p>Style me</p> element in the provided React app.
 
-import { useState } from "react";
-
 // The class should be applied when the <button> is clicked for the first time.
 
 // don't change the Component name "App"
+import { useState } from "react";
+
 export default function App() {
   const [isActive, setIsActive] = useState(false);
 
